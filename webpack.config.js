5e45dcfca07123b7116bc6df00e786c166220736
Avindra Fernando <avindra1@gmail.com>
2017-12-10 20:47:28 -0600

    Webpack Demo - Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack')
+const path = require('path')
+const extractCommons = new webpack.optimize.CommonsChunkPlugin({
+    name: 'commons',
+    filename: 'commons.js'
+})
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const extractCSS = new ExtractTextPlugin('[name].bundle.css')
+
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: './app.js',
+    admin: './admin.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src'),
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }]
+    }, {
+      test: /\.scss$/,
+      loader: extractCSS.extract(['css-loader','sass-loader'])
+    }, {
+      test: /\.(png|jpg)$/,
+      use: [{
+        loader: 'url-loader',
+        options: { limit: 10000 } // Convert images < 10k to base64 strings
+      }]
+    }]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    extractCommons,
+    extractCSS
+  ]
+}
+
+module.exports = config
